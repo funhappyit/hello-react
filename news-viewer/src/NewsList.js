139edf0b09df31a5b0ef4eeb31f2dfe33ae30428
한유경 <yugyeong.han@seculayer.com>
2024-07-10 18:07:07 +0900

    2024-07-08 15장 까지

--- news-viewer/src/NewsList.js
+++ news-viewer/src/NewsList.js
@@ -20,9 +20,7 @@ const NewsListBlock = styled.div`
 const NewsList = ({category}) =>{
     const[loading,response,error] = usePromise(()=>{
         const query = category === 'all'?'': `&category=${category}`;
-        return axios.get(
-            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=fdb13d51715c43bcb72d866c6a54a68e`,
-        );
+
     },[category]);
 
     //대기 중일 때
