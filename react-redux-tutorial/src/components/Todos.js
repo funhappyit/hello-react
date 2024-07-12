4c95d0bdba67bab3a6ceb84a382e1305d05cd1f4
한유경 <yugyeong.han@seculayer.com>
2024-07-12 15:48:50 +0900

    2024-07-12 17장 커밋 푸시

--- react-redux-tutorial/src/components/Todos.js
+++ react-redux-tutorial/src/components/Todos.js
@@ -9,7 +9,7 @@ const TodoItem = ({todo,onToggle,onRemove}) => {
 };
 const Todos = ({
     input, //인풋에 입력되는 텍스트
-    todos,
+    todos, //할 일 목록이 들어 있는 객체
     onChangeInput,
     onInsert,
     onToggle,
@@ -21,11 +21,19 @@ const Todos = ({
     return(
         <div>
             <form onSubmit={onSubmit}>
-
+                <input/>
+                <button type="submit">등록</button>
             </form>
-
+            <div>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+            </div>
         </div>
     );
 };
 
 
+export default Todos;
