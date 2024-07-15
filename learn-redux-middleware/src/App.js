9ac0a0042ecb4b2cd6c9842f69c5bc49eab75ce1
한유경 <yugyeong.han@seculayer.com>
2024-07-15 15:54:31 +0900

    2024-07-15 18장 커밋 푸시

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterContainer from "./containers/CounterContainer";
+
+const App = () => {
+  return(
+      <div>
+        <CounterContainer/>
+      </div>
+  )
+}
+export default App;
