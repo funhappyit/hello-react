40fdb81d263436bc0d7d8c93016f1f5587f4dcc4
한유경 <yugyeong.han@seculayer.com>
2024-07-16 18:19:13 +0900

    2024-07-16 18장 커밋 푸시

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -3,15 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import rootReducer from "./modules";
 import {Provider} from 'react-redux';
+import loggerMiddleware from "./lib/loggerMiddleware";
+import {createLogger} from 'redux-logger';
+import { thunk } from 'redux-thunk';
+//
+// const store = createStore(rootReducer,applyMiddleware(loggerMiddleware));
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <Provider store={store}>
+//         <App/>
+//     </Provider>
+// );
 
-const store = createStore(rootReducer);
+// const logger = createLogger();
+// const store = createStore(rootReducer,applyMiddleware(logger));
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <Provider store={store}>
+//         <App/>
+//     </Provider>
+//
+// )
+
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger,thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
+  <Provider store={store}>
+      <App/>
+  </Provider>
 );
