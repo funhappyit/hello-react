853f2cd906b603db66e64f9fb7ed59722f1cd96f
한유경 <yugyeong.han@seculayer.com>
2024-07-18 18:03:31 +0900

    2024-07-18 18장

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
 
 const App = () => {
   return(
--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -2,40 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {applyMiddleware, createStore} from "redux";
-import rootReducer from "./modules";
-import {Provider} from 'react-redux';
-import loggerMiddleware from "./lib/loggerMiddleware";
-import {createLogger} from 'redux-logger';
-import { thunk } from 'redux-thunk';
-//
-// const store = createStore(rootReducer,applyMiddleware(loggerMiddleware));
-//
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <Provider store={store}>
-//         <App/>
-//     </Provider>
-// );
-
-// const logger = createLogger();
-// const store = createStore(rootReducer,applyMiddleware(logger));
-//
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <Provider store={store}>
-//         <App/>
-//     </Provider>
-//
-// )
+import { applyMiddleware, createStore } from "redux";
+import rootReducer, { rootSaga } from "./modules";
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import {thunk} from 'redux-thunk'; // 중괄호 없이 가져와야 합니다.
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger,thunk));
-
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger,thunk,sagaMiddleware)
+);
+sagaMiddleware.run(rootSaga);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
       <App/>
   </Provider>
 );
+
+
+
