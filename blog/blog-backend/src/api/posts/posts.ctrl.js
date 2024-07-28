42d04f069ab393603decd7c60d0945540cc0c26f
“funhappyit” <fun.happyit@gmail.com>
2024-07-28 20:31:07 +0900

    2024-07-28 22장까지

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,132 +1,138 @@
+import Post from '../../models/post';
+import mongoose from "mongoose";
+
+const {ObjectId} = mongoose.Types;
+
+export const checkObjectId = (ctx,next)=> {
+    const {id} = ctx.params;
+    if(!ObjectId.isValid(id)){
+        ctx.status = 400; //Bad Request
+        return;
+    }
+    return next();
+};
+
 let postId = 1; // id의 초깃값입니다.
 
-//posts 배열 초기 데이터
+// posts 배열 초기 데이터
 const posts = [
     {
-     id:1,
-     title:'제목',
-     body: '내용',
+        id: 1,
+        title: '제목',
+        body: '내용',
     },
 ];
+
 /*
 포스트 작성
 POST /api/posts
 {title,body}
 */
+export const write = async ctx => {
+    // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
+    const { title, body,tags } = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags
+    });
+    try{
+        await post.save();
+        ctx.body = post;
+    }catch (e){
+        ctx.throw(500,e);
+    }
+};
 
-exports.write = ctx => {
-    //REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-    const {title,body} = ctx.request.body;
-    postId +=1; // 기존 postId 값에 1을 더합니다.
-    const post = {id: postId, title, body};
-    posts.push(post);
-    ctx.body = post;
-}
 /*
 포스트 목록 조회
 GET /api/posts
 */
-exports.list = ctx => {
-    ctx.body = posts;
+export const list = async ctx => {
+    try {
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    }catch (e){
+        ctx.throw(500,e);
+    }
 };
+
 /*
 특정 포스트 조회
- GET /api/posts/:id
+GET /api/posts/:id
 */
-exports.read = ctx => {
-  const {id} = ctx.params;
-  /*
-  주어진 id 값으로 포스트를 찾습니다.
-  파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
-  비교할 p.id 값을 문자열로 변경해야 합니다.
-  */
-  const post = posts.find(p=>p.id.toString() === id);
-  //포스트가 없으면 오류를 반환합니다.
-  if(!post){
-      ctx.status = 404;
-      ctx.body = {
-        message:'포스트가 존재하지 않습니다.',
-      };
-      return;
-  }
-  ctx.body = post;
+export const read = async ctx => {
+    const { id } = ctx.params;
+    try{
+        const post = await Post.findById(id).exec();
+        if(!post){
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    }catch (e) {
+        ctx.throw(500, e);
+    }
 };
+
 /*
 특정 포스트 제거
-   DELETE /api/posts/:id
+DELETE /api/posts/:id
 */
-exports.remove = ctx => {
-    const {id} = ctx.params;
-    //해당 id를 가진 post가 몇 번째인지 확인합니다
-    const index = posts.findIndex(p=>p.id.toString() === id);
-    //포스트가 없으면 오류를 반환합니다.
-    if(index === -1){
+export const remove = async ctx => {
+    const { id } = ctx.params;
+    try{
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204;
+    }catch (e){
+        ctx.throw(500,e);
+    }
+};
+
+/*
+포스트 수정 (전체)
+PUT /api/posts/:id
+{title,body}
+*/
+export const replace = ctx => {
+    // put 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용합니다.
+    const { id } = ctx.params;
+    // 해당 id를 가진 post가 몇 번째인지 확인합니다.
+    const index = posts.findIndex((p) => p.id.toString() === id);
+    // 포스트가 없으면 오류를 반환합니다.
+    if (index === -1) {
         ctx.status = 404;
         ctx.body = {
             message: '포스트가 존재하지 않습니다.',
         };
         return;
     }
-    //index번째 아이템을 제거합니다.
-    posts.splice(index,1);
-    ctx.status = 204;
+    posts[index] = {
+        id: parseInt(id), // Ensure id is an integer
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
 };
+
 /*
-put /api/posts/:id
+포스트 수정 (특정 필드 변경)
+PATCH /api/posts/:id
 {title,body}
 */
-
-exports.replace = ctx => {
-    //put 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용합니다.
-    const{id} = ctx.params;
-    //해당 id를 가진 post가 몇 번째인지 확인합니다.
-    const index = posts.findIndex(p=>p.id.toString() === id);
-    //포스트가 없으면 오류를 반환합니다.
-    if(index === -1){
-        ctx.status = 404;
-        ctx.body = {
-            message:'포스트가 존재하지 않습니다.',
-        };
-        return;
+export const update = async ctx => {
+    // PATCH 메서드는 주어진 필드만 교체합니다.
+    const { id } = ctx.params;
+    try{
+        const post =  await Post.findByIdAndUpdate(id,ctx.request.body,{
+            new:true, //이 값을 설정하면 업데이트된 데이터를 반환합니다.
+            //false일 때는 업데이트되기 전의 데이터를 반환합니다.
+        }).exec();
+        if(!post){
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    }catch (e) {
+        ctx.throw(500,e);
     }
-    posts[index] = {
-        id,
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
 };
-
-/*포스트 수정(특정 필드 변경)
-  PATCH /api/posts/:id
-  {title,body}
-*/
-exports.update = ctx => {
-    //PATCH 메서드는 주어진 필드만 교체합니다.
-    const{id} = ctx.params;
-    //해당 id를 가진 post가 몇 번째인지 확인합니다.
-    const index = posts.findIndex(p=>p.id.toString() === id);
-    //포스트가 없으면 오류를 반환합니다.
-    if(index === -1){
-        ctx.status = 404;
-        ctx.body = {
-            message:'포스트가 존재하지 않습니다.',
-        };
-        return;
-    }
-    //기존 값에 정보를 덮어 씌웁니다.
-    posts[index] = {
-        ...posts[index],
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
-};
-
-
-
-
-
-
-
-
-
-
---
+++ blog/blog-backend/src/main.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import api from './api/index.js';
+
+// dotenv 설정
+dotenv.config();
+
+const { PORT, MONGO_URI } = process.env;
+
+// MongoDB 연결
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(e => {
+        console.error(e);
+    });
+
+// Koa 애플리케이션 생성
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // API 라우터 적용
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => {
+    console.log('Listening to port %d', port);
+});
